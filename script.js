855c5c60cf61873fee1fcc3334be5bbd9c64966a
pratiksingh27 <pratik27march@gmail.com>
2022-03-30 22:16:55 +0530

    added test page, work on desigining, formate of document

--- script.js
+++ script.js
@@ -24,16 +24,6 @@ window.onscroll = function(){
         up.style.display ="block";
     }
 }
-//text typing effect
-var i = 0;
-var txt = "Join us and learn from the best teachers of INDIA...";
-var speed =250;
-    function type(){
-        document.getElementById("pratik").innerHTML+= txt.charAt(i);
-        i++;
-        setTimeout(type,speed);
-    }
-    type();
 //prelodar
 var loader =document.getElementById("prelodar");
 window.addEventListener('load', function(){
@@ -41,17 +31,158 @@ window.addEventListener('load', function(){
 });
 // AOS.init();
 
+// Test part
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "javascript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "javascript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+
+
+];
+
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+
+       currentQuiz++
+
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
+
+
+
 
 //review
-// const post =document.querySelector(".post")
-// const exit = document.querySelector(".exit")
-// const widget = document.querySelector(".star-widget")
-// const edit = document.querySelector(".edit")
+// const post = document.querySelector(".post")
+//         const exit = document.querySelector(".exit")
+//         const widget = document.querySelector(".star-widget")
+//         const edit = document.querySelector(".edit")
 
-// post.onclick = ()=>{
-//     widget.style.display="none";
-    // exit.style.display= "block";
-//     console.log("my")
-//     return false;
-
-// }
+//         post.onclick = () => {
+//             widget.style.display = "none";
+//             exit.style.display = "block";
+//             console.log("my");
+//             return false;
+//         }
+//         edit.onclick = ()=>{
+//             widget.style.display = "block";
+//             exit.style.display = "none";
+//             console.log("my");
+//             return false;
+//         }
